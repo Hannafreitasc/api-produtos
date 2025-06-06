be17cac9d47586026a27327d21a52e4bd9241df7
hannafreitasc <hannafreitascc@gmail.com>
2025-06-06 12:17:01 -0300

    oo

---
+++ src/routes/produtos.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const produtosController = require('../controllers/produtos')
+const produtosMiddleware = require('../middlewares/produtos')
+const authMiddleware = require('../middlewares/auth')
+
+router.get('/produtos',
+ authMiddleware.validateToken,
+ produtosController.getProdutos)
+router.post(
+    '/produtos',
+    produtosMiddleware.validateCreateProduto,
+    produtosController.createProduto
+)
+router.delete(
+    '/produtos/:id',
+    produtosMiddleware.validateDeleteProduto,
+    produtosController.deleteProduto
+)
+
+module.exports = router;
